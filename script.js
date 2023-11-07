4ede524dad40c5f6b410ad8593cf97aa967caa75
MikiasTesfayeJ <Mikiastesfaye998@gmail.com>
2023-11-07 22:09:48 +0300

    added functionality: multiple tasks, clears input field after entry

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ const input = document.querySelector('.in > input');
 const add = document.querySelector('.in > button');
 
 let count = 0;          // to count the number of tasks
-
+let roaster = [];
+let list_string = '';
 
 add.addEventListener('click', () => {
-    output.innerText = input.value;
+    roaster.push(input.value);
+    list_string += ':-' + roaster[roaster.length-1] + '\n';
+    output.innerText = list_string;
     input.value = '';
 });
